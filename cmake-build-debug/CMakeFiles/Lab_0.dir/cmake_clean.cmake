4fb2f10dba7cea84edc8e57ce8118a350e5d47df
Alex <alex.torra01@estudiant.upf.edu>
2023-04-26 10:15:50 +0200

    he afegit l'opcio de guardar les dades dels usuaris registrats a un document de text

---
+++ cmake-build-debug/CMakeFiles/Lab_0.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_0.dir/main.c.obj"
+  "CMakeFiles/Lab_0.dir/main.c.obj.d"
+  "CMakeFiles/Lab_0.dir/menu.c.obj"
+  "CMakeFiles/Lab_0.dir/menu.c.obj.d"
+  "CMakeFiles/Lab_0.dir/user.c.obj"
+  "CMakeFiles/Lab_0.dir/user.c.obj.d"
+  "Lab_0.exe"
+  "Lab_0.exe.manifest"
+  "Lab_0.pdb"
+  "libLab_0.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Lab_0.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Lab_0.
+# This may be replaced when dependencies are built.
