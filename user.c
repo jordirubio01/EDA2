972a2da8280088eba293c37adab21e001add50c6
marcrodriiguez7 <marc.rodriguez08@estudiant.upf.edu>
2023-04-26 09:45:13 +0200

    age por birth_date

--- user.c
+++ user.c
@@ -6,13 +6,13 @@
 * Post: Los datos del usuario están completos
 */
 int fill_profile(User* user){
-    char name[20], username[20], email[50], location[20]; int age;
+    char name[20], username[20], email[50], location[20]; int birth_date;
     printf("Introduce tu nombre real:\n");
     scanf("%s", name);
     printf("Nombre de usuario:\n");
     scanf("%s", username);
     printf("Edad:\n");
-    scanf("%d", &age);
+    scanf("%d", &birth_date);
     printf("Email:\n");
     scanf("%s", email);
     printf("Ciudad:\n");
