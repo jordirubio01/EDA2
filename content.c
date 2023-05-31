70246ff0c704eeb9b77b06757eeb83e0b8f15d14
jordi <jordi.rubio01@estudiant.upf.edu>
2023-05-31 10:10:12 +0200

    Sesión 17/5/2023. @marcrodriiguez7, @jordirubio01, @atorra11. ·menu.c/h: primera opción disponible (iniciar sesión con contraseña). ·user.c/h: función save_user() completada e implementada en fill_profile(); nuevas funciones valid_login(), request_friend(), view_requests(). ·dictionary.c/h: nuevas estructuras Dictionary y Element; nuevas funciones create_dictionary() y add_element_as_first(). ·"requests.txt": nuevo fichero para solicitudes de amistad.

--- content.c
+++ content.c
@@ -1,2 +1,16 @@
+#include <malloc.h>
 #include "content.h"
+#include "string.h"
 
+/*ActivityLinked* make_activity_linked(char name[MAX_LENGTH], char type[MAX_LENGTH], char location[MAX_LENGTH], char schedule[MAX_LENGTH], char review[MAX_LENGTH_REVIEW], int stars, double price, User* user, ActivityLinked* first){
+    ActivityLinked* c = (ActivityLinked*) malloc(sizeof(ActivityLinked));
+    strcpy(c->activity->name, name);
+    strcpy(c->activity->type, type);
+    strcpy(c->activity->location, location);
+    strcpy(c->activity->schedule);
+    strcpy(c->activity->review);
+    c->activity->stars = stars
+    c->activity->price = price
+    c->activity->user = user
+
+}*/
--- content.h
+++ content.h
@@ -1,14 +1,24 @@
+#include "user.h"
 #ifndef LAB_0_CONTENT_H
 #define LAB_0_CONTENT_H
-
+#define MAX_LENGTH_REVIEW 1000
 #endif //LAB_0_CONTENT_H
 
 // Estructura de actividades
 typedef struct{
+    char name;
     char type;      // Monumento, Gastronomía, Hospedaje, Deporte, Transporte, Otros
     char location;  // Ubicación
     char schedule;  // Horarios
     double price;   // Precio
     int stars;      // Valoración (0-5)
     char review;    // Reseña, Opiniones, Comentarios
+    User* user;     // Usuario
 } Activity;
+
+typedef struct activity_linked_list{
+    Activity* activity;
+    struct activity_linked_list* next;
+}ActivityLinked;
+
+//ActivityLinked* make_activity_linked(char name[MAX_LENGTH] char type[MAX_LENGTH], char location[MAX_LENGTH], char schedule[MAX_LENGTH], char review[MAX_LENGTH_REVIEW], int stars, double price, User* user, UserLinked* first);
